415151feb3b5ace669a61cc9fcc4e6995bd79155
Diosney <diosney.s@gmail.com>
2014-04-09 14:21:33 -0400

    Initial commit.

---
+++ app.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+// TODO: Use only `director` since `flatiron` is too much overload.
+var path = require('path');
+var flatiron = require('flatiron');
+
+var commands = require('./lib/commands');
+
+var app = flatiron.app;
+
+app.version = require('./package.json').version;
+app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
+
+app.use(flatiron.plugins.cli, {
+//	source : path.join(__dirname, 'lib', 'commands'),
+//	usage  : 'Empty Flatiron Application, please fill out commands',
+	version: true
+});
+
+// Initialize commands.
+commands.init(app);
+
+app.start(function (error) {
+	process.exit(error ? 1 : 0);
+});
