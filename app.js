5dad3a05adbee1f038e4a1bf8ec9072a234f40e1
Diosney <diosney.s@gmail.com>
2014-04-10 11:08:40 -0400

    Added system/logging support.

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ app.version = require('./package.json').version;
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 app.use(flatiron.plugins.cli, {
-//	source : path.join(__dirname, 'lib', 'commands'),
-//	usage  : 'Empty Flatiron Application, please fill out commands',
 	version: true
 });
 
