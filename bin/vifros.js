52e04a6eb703702a0b7775b718a4a4234ba18726
Diosney <diosney.s@gmail.com>
2014-09-16 22:42:44 -0400

    * Added optimization to improve completion time by using a `--compgen` argument detection flag. * Moved `completion.js` file to the `lib` root.

--- bin/vifros.js
+++ bin/vifros.js
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
+/*
+ * Checks the command line arguments for presence of `compgen` (completion generation)
+ * argument, which is the way `complete` module generates command completion to
+ * be seed to bash.
+ *
+ * If this option is found, then the CLI app will terminate successfully since
+ * no further actions will needed (it was executed only to gather completion info).
+ *
+ * This is an optimization to gain in speed for both completion and command execution
+ * workflows.
+ *
+ */
+if (process.argv.indexOf('--compgen') != -1) {
+  // Load command completions.
+  require('./../lib/completion');
+
+  // Terminate the app successfully.
+  process.exit(0);
+}
+
 // TODO: Migrate to only `director` since `flatiron` is too much overload.
 var path = require('path');
 var flatiron = require('flatiron');
@@ -9,14 +29,12 @@ var commands = require('../lib/commands');
 var app = flatiron.app;
 
 app.version = require('./../package.json').version;
+// TODO: The config line is really needed? It is somehow being used?
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 app.use(flatiron.plugins.cli, {
   version: true
 });
-
-// Load command completions.
-require('./../lib/common/completion');
 
 // Initialize commands.
 commands.init(app);
